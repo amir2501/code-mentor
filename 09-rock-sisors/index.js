a64c22a691eaee4f642a5dde23d7d49dda5d6f81
Erkinov Amirbek <erkinovamir100@gmail.com>
2024-06-09 09:31:08 +0500

    alomst finished version

--- 09-rock-sisors/index.js
+++ 09-rock-sisors/index.js
@@ -5,6 +5,17 @@ let option  = document.getElementById("option")
 let clicked = document.getElementById("clicked");
 let random_img = document.getElementById("random_img");
 let user_choose = document.getElementById("user_choose");
+let score = document.getElementById("score");
+let play_again = document.getElementById("play_again");
+let user_chose_li = document.getElementById("user_chose_li");
+let computer_chose_li = document.getElementById("computer_chose_li");
+let win_status = false;
+let starter_count = 12;
+let user_status = document.getElementById("user_status");
+let status = "";
+
+score.innerHTML = `${starter_count}`
+
 
 
 //
@@ -17,59 +28,87 @@ let clicked_element;
 // random_img.src = "./images/icon-scissors.svg"
 
 
-first.addEventListener("click", function() {
+first.addEventListener("click", () => {
    clicked_element = 1;
-    check()
+    check(clicked_element)
 });
 
-second.addEventListener("click", function() {
+second.addEventListener("click", () => {
      clicked_element = 2;
-    check()
+    check(clicked_element)
 })
 
-third.addEventListener("click", function() {
+third.addEventListener("click", () => {
      clicked_element = 3;
-    check()
+    check(clicked_element)
 })
 
-function check(){
-    switch (clicked_element){
+
+function check_winner(){
+    let computer_chose =  Math.floor(Math.random() * 3);
+
+    computer_chose_li.classList.contains("yellow") && computer_chose_li.classList.remove("yellow");
+    computer_chose_li.classList.contains("red") && computer_chose_li.classList.remove("red");
+    computer_chose_li.classList.contains("blue") && computer_chose_li.classList.remove("blue");
+
+
+    computer_chose === 0 && (random_img.src = "./images/icon-paper.svg") && (computer_chose_li.classList.add("blue"));
+    computer_chose === 1 && (random_img.src = "./images/icon-scissors.svg") && (computer_chose_li.classList.add("yellow"));
+    computer_chose === 2 && (random_img.src = "./images/icon-rock.svg") && (computer_chose_li.classList.add("red"));
+
+    (clicked_element === 1 &&  computer_chose === 0) && (win_status = "draw") ;
+    (clicked_element === 1 &&  computer_chose === 1) && (win_status = false);
+    (clicked_element === 1 &&  computer_chose === 2) && (win_status = true);
+    (clicked_element === 2 &&  computer_chose === 0) && (win_status = true);
+    (clicked_element === 2 &&  computer_chose === 1) && (win_status = "draw");
+    (clicked_element === 2 &&  computer_chose === 2) && (win_status = false);
+    (clicked_element === 3 &&  computer_chose === 0) && (win_status = false);
+    (clicked_element === 3 &&  computer_chose === 1) && (win_status = true);
+    (clicked_element === 3 &&  computer_chose === 2) && (win_status = "draw");
+    (win_status && win_status !== "draw") && starter_count++;
+    (!win_status && win_status !== "draw") && starter_count--;
+    (win_status === "draw") && ( status = "Draw");
+    (win_status === true) && ( status = "You win");
+    (win_status === false) && ( status = "You lose");
+    user_status.innerHTML = `${status}`
+    score.innerHTML = `${starter_count}`;
+    console.log(computer_chose);
+}
+
+play_again.addEventListener("click", function (){
+    option.classList.remove("hidden");
+    clicked.classList.add("hidden");
+});
+
+function check(element) {
+    switch (element){
         case 1:
             option.classList.add("hidden");
             clicked.classList.remove("hidden");
             user_choose.src = "./images/icon-paper.svg";
+            user_chose_li.classList.add("blue");
+            user_chose_li.classList.remove("yellow");
+            user_chose_li.classList.remove("red");
             check_winner();
             break;
         case 2:
             option.classList.add("hidden");
             clicked.classList.remove("hidden");
             user_choose.src = "./images/icon-scissors.svg";
+            user_chose_li.classList.add("yellow");
+            user_chose_li.classList.remove("blue");
+            user_chose_li.classList.remove("red");
             check_winner();
             break;
         case 3:
             option.classList.add("hidden");
             clicked.classList.remove("hidden");
             user_choose.src = "./images/icon-rock.svg";
+            user_chose_li.classList.add("red");
+            user_chose_li.classList.remove("yellow");
+            user_chose_li.classList.remove("blue");
             check_winner();
             break;
     }
-}
-
-function check_winner(){
-    let computer_chose =  Math.floor(Math.random() * 3);
-    computer_chose === 0 && random_img.src(".images/icon-paper.svg");
-    computer_chose === 2 && random_img.src(".images/icon-scissors.svg");
-    computer_chose === 3 && random_img.src(".images/icon-rock.svg");
-
-    clicked_element === 1 &&  computer_chose === 0 && alert("draw")
-    clicked_element === 1 &&  computer_chose === 1 && alert("lose")
-    clicked_element === 1 &&  computer_chose === 2 && alert("win")
-    clicked_element === 2 &&  computer_chose === 0 && alert("win")
-    clicked_element === 2 &&  computer_chose === 1 && alert("draw")
-    clicked_element === 2 &&  computer_chose === 2 && alert("lose")
-    clicked_element === 3 &&  computer_chose === 0 && alert("lose")
-    clicked_element === 3 &&  computer_chose === 1 && alert("win")
-    clicked_element === 3 &&  computer_chose === 2 && alert("draw")
-
 }
 
