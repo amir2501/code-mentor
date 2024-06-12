8f9dcd341265d156347f6e058238801da3f30935
Erkinov Amirbek <erkinovamir100@gmail.com>
2024-06-12 18:33:46 +0500

    finished version

--- 09-rock-sisors/index.js
+++ 09-rock-sisors/index.js
@@ -1,7 +1,7 @@
 let first = document.getElementById("1");
 let second = document.getElementById("2");
 let third = document.getElementById("3");
-let option  = document.getElementById("option")
+let option = document.getElementById("option")
 let clicked = document.getElementById("clicked");
 let random_img = document.getElementById("random_img");
 let user_choose = document.getElementById("user_choose");
@@ -15,39 +15,27 @@ let user_status = document.getElementById("user_status");
 let status = "";
 let user_won = document.getElementById("user_won");
 let computer_won = document.getElementById("computer_won");
-
 score.innerHTML = `${starter_count}`
-
-
-
-//
-// let paper = 0;
-// let rock = 1;
-// let scissors = 2;
-
 let clicked_element;
-//
-// random_img.src = "./images/icon-scissors.svg"
-
 
 first.addEventListener("click", () => {
-   clicked_element = 1;
+    clicked_element = 1;
     check(clicked_element)
 });
 
 second.addEventListener("click", () => {
-     clicked_element = 2;
+    clicked_element = 2;
     check(clicked_element)
 })
 
 third.addEventListener("click", () => {
-     clicked_element = 3;
+    clicked_element = 3;
     check(clicked_element)
 })
 
 
-function check_winner(){
-    let computer_chose =  Math.floor(Math.random() * 3);
+function check_winner() {
+    let computer_chose = Math.floor(Math.random() * 3);
 
     computer_chose_li.classList.contains("yellow") && computer_chose_li.classList.remove("yellow");
     computer_chose_li.classList.contains("red") && computer_chose_li.classList.remove("red");
@@ -56,37 +44,36 @@ function check_winner(){
     user_won.classList.contains("gradient") && user_won.classList.remove("gradient");
 
 
-
     computer_chose === 0 && (random_img.src = "./images/icon-paper.svg") && (computer_chose_li.classList.add("blue"));
     computer_chose === 1 && (random_img.src = "./images/icon-scissors.svg") && (computer_chose_li.classList.add("yellow"));
     computer_chose === 2 && (random_img.src = "./images/icon-rock.svg") && (computer_chose_li.classList.add("red"));
 
-    (clicked_element === 1 &&  computer_chose === 0) && (win_status = "draw") ;
-    (clicked_element === 1 &&  computer_chose === 1) && (win_status = false);
-    (clicked_element === 1 &&  computer_chose === 2) && (win_status = true);
-    (clicked_element === 2 &&  computer_chose === 0) && (win_status = true);
-    (clicked_element === 2 &&  computer_chose === 1) && (win_status = "draw");
-    (clicked_element === 2 &&  computer_chose === 2) && (win_status = false);
-    (clicked_element === 3 &&  computer_chose === 0) && (win_status = false);
-    (clicked_element === 3 &&  computer_chose === 1) && (win_status = true);
-    (clicked_element === 3 &&  computer_chose === 2) && (win_status = "draw");
+    (clicked_element === 1 && computer_chose === 0) && (win_status = "draw");
+    (clicked_element === 1 && computer_chose === 1) && (win_status = false);
+    (clicked_element === 1 && computer_chose === 2) && (win_status = true);
+    (clicked_element === 2 && computer_chose === 0) && (win_status = true);
+    (clicked_element === 2 && computer_chose === 1) && (win_status = "draw");
+    (clicked_element === 2 && computer_chose === 2) && (win_status = false);
+    (clicked_element === 3 && computer_chose === 0) && (win_status = false);
+    (clicked_element === 3 && computer_chose === 1) && (win_status = true);
+    (clicked_element === 3 && computer_chose === 2) && (win_status = "draw");
     (win_status && win_status !== "draw") && starter_count++;
     (!win_status && win_status !== "draw") && starter_count--;
-    (win_status === "draw") && ( status = "Draw") && (user_won.classList.remove("gradient")) && (computer_won.classList.remove("gradient"));
-    (win_status === true) && ( status = "You win") && (user_won.classList.add("gradient"));
-    (win_status === false) && ( status = "You lose") && (computer_won.classList.add("gradient"));
+    (win_status === "draw") && (status = "Draw") && (user_won.classList.remove("gradient")) && (computer_won.classList.remove("gradient"));
+    (win_status === true) && (status = "You win") && (user_won.classList.add("gradient"));
+    (win_status === false) && (status = "You lose") && (computer_won.classList.add("gradient"));
     user_status.innerHTML = `${status}`
     score.innerHTML = `${starter_count}`;
     console.log(computer_chose);
 }
 
-play_again.addEventListener("click", function (){
+play_again.addEventListener("click", function () {
     option.classList.remove("hidden");
     clicked.classList.add("hidden");
 });
 
 function check(element) {
-    switch (element){
+    switch (element) {
         case 1:
             option.classList.add("hidden");
             clicked.classList.remove("hidden");
